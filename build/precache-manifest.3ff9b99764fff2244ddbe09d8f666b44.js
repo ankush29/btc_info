17538f84aabc259677420729d0402524eecb391f
ankush29 <saini.ankush29@gmail.com>
2019-08-03 20:20:30 +0530

    btc project loading

---
+++ build/precache-manifest.3ff9b99764fff2244ddbe09d8f666b44.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf730fc8eaf7f2e9706c",
+    "url": "/static/js/btcChart.f1085450.chunk.js"
+  },
+  {
+    "revision": "63b8252ed0be9d289ca3",
+    "url": "/static/js/findBtc.8b098f31.chunk.js"
+  },
+  {
+    "revision": "acf05126c490d65f18bc",
+    "url": "/static/css/main.d7da9399.chunk.css"
+  },
+  {
+    "revision": "acf05126c490d65f18bc",
+    "url": "/static/js/main.120bec8c.chunk.js"
+  },
+  {
+    "revision": "a96fbf2abf7d8f99a7bd",
+    "url": "/static/js/runtime~main.3a5bdb38.js"
+  },
+  {
+    "revision": "8292d075e85652497b68",
+    "url": "/static/js/4.88f5dc3d.chunk.js"
+  },
+  {
+    "revision": "ec7a177b2bea69416668",
+    "url": "/static/js/5.2a5406e2.chunk.js"
+  },
+  {
+    "revision": "747286504139eab9c181",
+    "url": "/static/js/6.c9a842cf.chunk.js"
+  },
+  {
+    "revision": "beb51f087a98eacb8253af92f1b65e7c",
+    "url": "/index.html"
+  }
+];
